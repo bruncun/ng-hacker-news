eed77c2a6546ebd996db9205fdacc4d8f99688cb
bruncun <bruncun@Brunos-iMac.attlocal.net>
2017-06-04 13:37:54 -0400

    Add pagination

--- app/top-stories/top-stories.js
+++ app/top-stories/top-stories.js
@@ -12,19 +12,27 @@ angular
     }
   ])
   .controller("TopStoriesCtrl", [
+    "$location",
     "TopStoriesService",
-    function(TopStoriesService) {
+    function($location, TopStoriesService) {
       var vm = this;
+      vm.currentPage = $location.search().page || 1;
       vm.stories = [];
       vm.loading = true;
+      vm.start = 30 * (vm.currentPage - 1) + 1;
 
-      activate();
+      vm.updatePage = function(newPageNumber, oldPageNumber) {
+        vm.start = 30 * (newPageNumber - 1) + 1;
+        $location.search("page", newPageNumber);
+      };
 
       function activate() {
         TopStoriesService.getStories().then(function(res) {
           vm.loading = false;
-          vm.stories = res.data.slice(0, 30);
+          vm.stories = res.data;
         });
       }
+
+      activate();
     }
   ]);
