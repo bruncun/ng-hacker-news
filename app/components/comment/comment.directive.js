2c61628f6a053c83333a01278a1fd61f82db9dfc
bruncun <bruncun@Brunos-iMac.attlocal.net>
2017-06-11 07:59:03 -0400

    Explicitly inject dependencies

--- app/components/comment/comment.directive.js
+++ app/components/comment/comment.directive.js
@@ -8,22 +8,27 @@ angular
         id: "=id"
       },
       templateUrl: "components/comment/comment.html",
-      controller: function($scope, $filter, TopStoriesService) {
-        var vm = this;
-        vm.loading = true;
+      controller: [
+        "$scope",
+        "$filter",
+        "TopStoriesService",
+        function($scope, $filter, TopStoriesService) {
+          var vm = this;
+          vm.loading = true;
 
-        activate();
+          activate();
 
-        function activate() {
-          TopStoriesService.getStory($scope.id).then(function(res) {
-            vm.commentText = res.data.text;
-            vm.commentAuthor = res.data.by;
-            vm.kids = res.data.kids;
-            vm.commentTime = $filter("getHoursAgo")(res.data.time);
-            vm.loading = false;
-          });
+          function activate() {
+            TopStoriesService.getStory($scope.id).then(function(res) {
+              vm.commentText = res.data.text;
+              vm.commentAuthor = res.data.by;
+              vm.kids = res.data.kids;
+              vm.commentTime = $filter("getHoursAgo")(res.data.time);
+              vm.loading = false;
+            });
+          }
         }
-      },
+      ],
       controllerAs: "vm",
       link: function(scope, elem, attrs, ctrl) {}
     };
